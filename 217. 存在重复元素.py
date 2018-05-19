fe8533b6ca763b73522248ed1233bde534416000
Jing Xinli <jingxl2116@gmail.com>
2018-05-19 18:57:01 +0800

    Create 217. 存在重复元素.py
    so easy

---
+++ 217. 存在重复元素.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        ans = set(nums)
+        if len(ans) == len(nums):
+            return False
+        else:
+            return True
